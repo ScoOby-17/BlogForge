8f98d9440b6d8fe09f2afda884596be7e48fa56d
suhaibAzim <suhaibigl@gmail.com>
2026-06-14 22:18:44 +0530

    fix errors

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,23 @@ const router = express.Router();
 
 const { auth } = require("../middlewares/auth.js");
 const { isOwner } = require("../middlewares/isOwner.js");
+const { isPostOwner } = require("../middlewares/isPostOwner.js")
 
-const { getUserProfile, updateUserProfile, deleteUserAccount, } = require("../controler/usersControler.js");
+const { getUserProfile, 
+  updateUserProfile, 
+  deleteUserAccount, 
+  getMyPosts, 
+  getMyPostById,
+  updateMyPost,
+  deleteMyPost} = require("../controler/usersControler.js");
+
+router.get("/me/posts", auth, getMyPosts);
+router.get("/me/posts/:postId", auth, isPostOwner, getMyPostById);
+router.put("/me/posts/:postId", auth, isPostOwner, updateMyPost);
+router.delete("/me/posts/:postId", auth, isPostOwner, deleteMyPost);
 
 router.get("/:id", auth, isOwner, getUserProfile);
 router.put("/:id", auth, isOwner, updateUserProfile);
 router.delete("/:id", auth, isOwner, deleteUserAccount);
-
-router.get("/me/posts", auth, getMyPosts);
-router.get("/me/posts/:postId", auth, isOwner, getMyPostById);
-router.put("/me/posts/:postId", auth, isOwner, updateMyPost);
-router.delete("/me/posts/:postId", auth, isOwner, deleteMyPost);
 
 module.exports = router;
